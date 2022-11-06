5747f033ba78e0dcfd46396a23af1058077acea1
amlfr <amartinez.lalenec@gmail.com>
2022-11-06 23:04:57 +0100

    Added script to populate the main page

---
+++ front/js/script.js
@@ -0,0 +1,72 @@
+/*Declaring variables*/
+const fetchUrl = "http://localhost:3000/api/products";
+let productsArray = [];
+const parentElt = document.getElementById("items");
+ 
+
+
+
+/** The function fetches the products from the API then uses the data to create
+ * html elements on the page
+ */
+async function fillProducts() {
+
+  /*Fetching the json products*/
+  let response = await fetch(fetchUrl);
+  if (response.ok) { 
+    products = await response.json();
+  } else {
+    /* alert("HTTP-Error: " + response.status);    TO DELETE*/
+  }
+
+  /*Using the products gotten to populate the html page*/
+  for (let product of products) {
+
+    const newAnchor = document.createElement('a');
+    parentElt.appendChild(newAnchor);
+    const hrefId = "./product.html?id=" + product._id;
+    newAnchor.setAttribute("href", hrefId);
+
+    const newArticle = document.createElement('article');
+    newAnchor.appendChild(newArticle);
+
+    const newImage = document.createElement('img');
+    newArticle.appendChild(newImage);
+    newImage.setAttribute("src", product.imageUrl);
+    newImage.setAttribute("alt", product.altTxt);
+
+    const newHeading = document.createElement('h3');
+    newArticle.appendChild(newHeading);
+    newHeading.setAttribute("class", "productName");
+    newHeading.innerHTML = product.name;
+
+    const newText = document.createElement('p');
+    newArticle.appendChild(newText);
+    newText.setAttribute("class", "productDescription");
+    newText.textContent = product.description; 
+
+
+
+  }
+};
+
+fillProducts();
+
+
+/* function fetchJson() {
+
+fetch(fetchUrl)
+.then(function(res) {
+  if (res.ok) {
+    return res.json();
+  }
+})
+.then(function(products) {
+  console.log(products);
+})
+.catch(function(err) {
+  alert("HTTP-Error: " + response.status);
+});
+}
+
+fetchJson(); */
---
+++ front/js/test.js
@@ -0,0 +1,7 @@
+let x = 3;
+
+function add() {
+    x = 4;
+}
+add();
+console.log(x);
